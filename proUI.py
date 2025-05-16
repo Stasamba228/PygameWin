497d1d7c3b13a981aa841885aeddb2e4831e282c
Stasamba228 <stanislavfedorishin@gmail.com>
2025-05-16 17:39:51 +0300

    proUI.py

--- proUI.py
+++ proUI.py
@@ -5,13 +5,10 @@ Window_size = 1000, 800
 window = display.set_mode(Window_size)
 clock = time.Clock()
 
-rect = Rect(0, 0, 100, 100)
-
 while True:
     for e in event.get():
         if e.type ==QUIT:
             quit()
-    draw.rect(window, (255, 0, 0), rect)
 
     display.update()
     clock.tick(60)
